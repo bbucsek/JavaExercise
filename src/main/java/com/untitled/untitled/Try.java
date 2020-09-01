36f434e0e34bd1b045c84e5a73d13ddf384158c1
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-09-01 22:19:28 +0200

    implement bubblesort

--- src/main/java/com/untitled/untitled/Try.java
+++ src/main/java/com/untitled/untitled/Try.java
@@ -1,9 +1,12 @@
 package com.untitled.untitled;
 
+import com.untitled.untitled.Algorithms.Sorting.BubbleSort;
 import com.untitled.untitled.DataStructures.DataBinarySearchTree;
 import com.untitled.untitled.DataStructures.DataHashMap;
 import com.untitled.untitled.DesignPatterns.Payment;
 import com.untitled.untitled.DesignPatterns.PaymentBuilder;
+
+import java.util.stream.Stream;
 
 public class Try {
 
@@ -28,8 +31,15 @@ public class Try {
 
     public static void main(String[] args) {
 
-        Payment payment = Payment.builder().loan(200).salary(500).tax(300).build();
-        System.out.println(payment.getLoan() + " " + payment.getSalary() + " " + payment.getTax());
+        //Payment payment = Payment.builder().loan(200).salary(500).tax(300).build();
+        //System.out.println(payment.getLoan() + " " + payment.getSalary() + " " + payment.getTax());
+
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] arr = { 5, 4, 3, 1, 2, 10, 6};
+        bubbleSort.sort(arr);
+        for (int num : arr) {
+            System.out.println(num);
+        }
     }
 
 }
